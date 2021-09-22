95e7487ea444c75ed597c21b06eeb94b50acc1e9
kevinmore26 <74431425+kevinmore26@users.noreply.github.com>
2021-09-21 22:19:36 -0500

    chore:"cambio de sqlite a postgres"

--- libreria/gestion/models.py
+++ libreria/gestion/models.py
@@ -3,7 +3,86 @@ from django.db import models
 #https://docs.djangoproject.com/en/3.2/ref/models/
 
 class ProductoModel(models.Model):
+
+    class OpcionesUm(models.TextChoices):
+        UNIDADES='UN','UNIDADES'
+        DOCENTA='DOC','DOCENA'
+        CIENTO='CI','CIENTO'
+        MILLAR='MI','MILLAR'
+
     productoId = models.AutoField(primary_key=True,null=False, unique=True, db_column='id')
 
     productoNombre= models.CharField(max_length=45,db_column='nombre',null=False)
+    # 
+    productoPrecio=models.DecimalField(max_digits=5,decimal_places=2,db_column='precio')
+    # 
+    productoUnidadMedida= models.TextField(choices=OpcionesUm.choices,default=OpcionesUm.UNIDADES,db_column='unidad_medida')
+
+
+    class Meta:
+        # indica el nombre de la tabla en la bd 
+        db_table='productos'
+        # ordering=> modifica el ordenamiento por defecto (x el id )al momento de devolver los registros 
+        # el "-" es para indicarle de mayor a menors
+        ordering=['-productoPrecio']
+
+        # verbose_name y .... => sirven para el panel administrativo de django
+        verbose_name='producto'
+        verbose_name_plural='productos'
+
+
+
+class ClienteModel(models.Model):
+    clienteId=models.AutoField(primary_key=True,null=False, unique=True, db_column='id')
+
+    clienteNombre= models.CharField(max_length=45,db_column='nombre',null=False)
+
+    clienteDocumento=models.CharField(max_length=12,db_column='documento',unique=True)
+
+    pclienteDireccion=models.CharField(max_length=100,db_column='direccion')
+
+    class Meta:
+        db_table='clientes'
+        verbose_name='cliente'
+        verbose_name_plural='clientes'
+
+class CabeceraModel(models.Model):
+    class OpcionesUm(models.TextChoices):
+        VENTA='V','VENTA'
+        COMPRA='C','COMPRA'
+
+        
+    cabeceraId=models.AutoField(primary_key=True,null=False, unique=True, db_column='id')
+    fechaCabecera=models.CharField(max_length=8,db_column='fecha',unique=True)
+    tipoCabecera= models.TextField(choices=OpcionesUm.choices,db_column='tipo')
+
+
+    # 
+    clientes=models.ForeignKey(to=ClienteModel, db_column='clientes_id',
+                               null=False,related_name='clienteCabecera',on_delete=models.PROTECT)
+    # 
+
+    class Meta:
+        db_table='cabecera_operaciones'
+        verbose_name='cabecera'
+        verbose_name_plural='cabeceras'
+
+class DetalleModel(models.Model):
+    detalleId=models.AutoField(db_column='id',primary_key=True,unique=True,null=False)
+
+    detalleCantidad=models.IntegerField(db_column='cantidad',null=False)
+
+    detalleImporte=models.DecimalField(max_digits=5,decimal_places=2,db_column='importe',null=False)
+    
+    productos=models.ForeignKey(to=ProductoModel,db_column='producto_id',on_delete=models.PROTECT,related_name='productoDetalles',null=False)
+
+    cabeceras=models.ForeignKey(to=CabeceraModel,db_column='cabecera_operaciones_id',on_delete=models.
+    PROTECT,related_name='cabeceraDetalles',null=False)
+    
+
+    class Meta:
+        db_table='detalle_operaciones'
+        verbose_name='detalle'
+        verbose_name_plural='detalles'
+
 
--- libreria/libreria/settings.py
+++ libreria/libreria/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gestion',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,15 @@ WSGI_APPLICATION = 'libreria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST':environ.get('DB_HOST'),
+        'NAME':environ.get('DB_NAME'),
+        'PASSWORD':environ.get('DB_PASSWORD'),
+        'PORT':environ.get('BD_PORT'),
+        'USER':environ.get('DB_USERNAME'),
+        'TEST':{
+            'NAME':environ.get('DB_TEST')
+        }
     }
 }
 
@@ -105,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Lima'
 
 USE_I18N = True
 

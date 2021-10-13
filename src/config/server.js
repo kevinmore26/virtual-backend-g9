242f738a4ed620d4ae3a42a14b1e8350d2faee51
kevinmore26 <74431425+kevinmore26@users.noreply.github.com>
2021-10-12 19:52:50 -0500

    ultimo jeje

--- src/config/server.js
+++ src/config/server.js
@@ -31,7 +31,7 @@ export class Server {
       }); 
     });
     if (process.env.NODE_ENV === "production") {
-      documentacion.host = "tareas-express-kevin.herokuapp.com/";
+      documentacion.host = "tareas-express-kevin.herokuapp.com";
       documentacion.schemes = ["https"];
     } else {
       documentacion.host = `127.0.0.1;${this.puerto}`;

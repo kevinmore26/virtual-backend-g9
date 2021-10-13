44186872ff3a5937e513a449eba0b6a50b5a077c
kevinmore26 <74431425+kevinmore26@users.noreply.github.com>
2021-10-12 19:46:59 -0500

    hola

--- src/config/server.js
+++ src/config/server.js
@@ -30,11 +30,13 @@ export class Server {
         message: "Bienvenido a mi API",
       }); 
     });
-    process.env.NODE_ENV === "production"
-    ? ((documentacion.host = "https://tareas-express-kevin.herokuapp.com/"),
-      (documentacion.schemes = ["https"]))
-    : ((documentacion.host = `http://127.0.0.1;${this.puerto}`),
-      (documentacion.schemes = ["http"]));
+    if (process.env.NODE_ENV === "production") {
+      documentacion.host = "https://tareas-express-eduardo.herokuapp.com/";
+      documentacion.schemes = ["https"];
+    } else {
+      documentacion.host = `http://127.0.0.1;${this.puerto}`;
+      documentacion.schemes = ["http"];
+    }
     this.app.use(tareasRouter);
     this.app.use('/docs', swagger.serve, swagger.setup(documentacion));
   }

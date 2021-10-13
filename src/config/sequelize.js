a6c380b4a124740b39406f9433fbfa59c83b9ae7
kevinmore26 <74431425+kevinmore26@users.noreply.github.com>
2021-10-12 19:37:12 -0500

    chore: cambios en el sqeuelize

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -4,5 +4,15 @@ require("dotenv").config();
 
 export const conexion = new Sequelize(
   process.env.DATABASE_URL,
-  { logging: false } // logging => indica si que quiere mostrar o no las consultas a la bd
+  { logging: false,
+  dialectOptions : 
+  process.env.NODE_ENV === "production"
+  ?{
+    ssl:{
+      rejectUnauthorized: false,
+    },
+    
+  }
+: {},
+} // logging => indica si que quiere mostrar o no las consultas a la bd,
 );

692a45f0645f97ba1ac603724621cfa21cb5f045
kevinmore26 <74431425+kevinmore26@users.noreply.github.com>
2021-10-20 12:57:12 -0500

    commit again

--- src/config/server.ts
+++ src/config/server.ts
@@ -11,7 +11,7 @@ export class Server {
   // private => no podra ser accedido desde fuera de la clase
   // readonly => no podra ser modificado su valor afuera del constructor
   private readonly app: Express;
-  private readonly puerto: number;
+  private readonly puerto: unknown;
 
   constructor() {
     this.app = express();
